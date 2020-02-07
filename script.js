c4cc47d196f335936111757f3969f2354486029e
cteichman123 <60764095+cteichman123@users.noreply.github.com>
2020-02-06 19:37:27 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,140 @@
+// step1
+
+// var someMonth;
+// function theMonth();
+// var currentMonth;
+// var summerMonth;
+// var mylibraryFunction;
+
+
+// //step2
+
+// numberic literal 2.0
+// Boolean literal true
+// literal null null
+
+
+// //step3
+
+// let anExpression = 3 * 10 + 8
+
+// let anExpression= 12 * 8 + 9
+
+//step4
+
+// let firstName, lastName, address, city, state, zipCode, yourAge, referralSource, mayWeContactYou
+
+// //step5
+
+// let firstName = "chad"; let lastName = "Teichman"; let address = "Misty lane"; let city = "San Diego"; let state = "California"; let zipCode = 92115; let yourAge = "29"; let referralSource = "web"; let mayWeContactYou = "no";
+
+// //step6
+
+// let age = 20;
+// window.console.log ("chad is" + age + "years old" )
+
+//step 7 
+
+// let occupation = analyst;
+// window.console.log ("chad is and" + occupation)
+
+
+// let timeWorking = 11 months
+// window.console.log ("Chad has worked for" + timeWorking )
+
+
+// step8
+
+// var someString = "Who once said, Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.";
+// window.console.log(someString);
+
+
+// step9
+let petType = "null";
+window.console.log (null)
+
+let yearofExperience ="undefined";
+window.console.log (undefined)
+
+step10
+
+typeof true  Boolean
+typeof (1,2,3,4) Object
+typeof myVehicle undefined
+typeof 2.3 Number
+
+step11
+let name = Zak Ruvalcaba;
+let Myclass= javascript;
+alert(Hello + "name" + welcome to the + "Myclass"+ class)
+
+step12
+let name = Chad
+
+step13
+let course = javascript
+
+step14
+alert(Hello + "name" + welcome to the + "Myclass"+ class)
+
+step15
+window.prompt ("please enter your name")
+
+Step16
+window.prompt ("what class are you taking?")
+
+step17
+let x= 10;
+
+let y = 20;
+window.console.log (x+=y)
+
+step18
+let  x = 20
+window.console.log (X+=20)
+
+step19
+let x = 20;
+window.console.log (x*=5)
+
+step20
+
+let x = 20/=3
+window.console.log (x/=1)
+
+step21
+
+let x = 100;
+let y = 100;
+window.console.log (x == y);
+
+step22
+let x = 500
+let y = 300
+window.console.log (x == y)
+
+step23
+typeof (widget)
+window.console.log ("widget")
+
+step24
+instanceof (widget)
+window.console.log ("widget")
+
+step25
+
+instaceof (widget);
+window.console.log (false)
+
+
+
+
+
+
+
+
+
+
+
+
+
